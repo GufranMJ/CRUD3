53841a901543a195f65a597662693732ba379eb0
GufranMJ <gufran.khirkeecollective@gmail.com>
2021-11-10 14:30:51 +0530

    Initial commit

---
+++ crud/crud/views.py
@@ -0,0 +1,10 @@
+###from django.shortcuts import render
+###from .forms import StudentRegistration
+
+#####def base(request):
+    #if request.method == 'POST':
+       # fm = StudentRegistration(request.POST)
+    #else:
+        #fm = StudentRegistration()
+    #return render(request, 'base.html', {'form':fm})##
+
